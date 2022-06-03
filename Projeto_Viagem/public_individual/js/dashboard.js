8769d8c4549d3b9af6acc8ad2e598275ac4c27e1
Gabrie de Miranda Mello <Gabriel.mmello@sptech.school>
2022-06-03 16:32:35 -0300

    finalizando dashboard

--- Projeto_Viagem/public_individual/js/dashboard.js
+++ Projeto_Viagem/public_individual/js/dashboard.js
@@ -55,10 +55,10 @@ function DadosGrafico() {
             resposta.json().then(function (resposta) {
                 var total = resposta[0].total_historia;
                 sessionStorage.TOTAL_HISTORIA = total
-                if (total < 5) {
+                if (total < 2) {
                     qtd_viagens.innerHTML = "Você Tem Poucas Viagens"
                     qtd_viagens.style.backgroundImage = "linear-gradient(#ce0303, #F2F2F2, #ce0303)"
-                } else if (total < 10) {
+                } else if (total < 5) {
                     qtd_viagens.innerHTML = "Você Tem Muitas Viagens"
                     qtd_viagens.style.backgroundImage = "linear-gradient(#2dd603, #F2F2F2, #2dd603)"
                 } else {
@@ -75,4 +75,34 @@ function DadosGrafico() {
         console.error(resposta);
     });
 
+}
+
+function CalcularGasto() {
+    var km_viagem = input_km_viagem.value
+    var km_carro = input_km_carro.value
+    var tipo_combustivel = select_tipo_combustivel.value
+
+    
+    var gasto_carro = km_viagem / km_carro
+    
+    var gasto_total_consumo = 0
+
+    if (tipo_combustivel == 1) {
+        var gasolina = gasto_carro * 6.93
+        gasto_total_consumo = gasolina
+    } else if (tipo_combustivel == 2) {
+        var alcool = gasto_carro * 4.95
+        gasto_total_consumo = alcool
+    } else {
+        var gnv = gasto_carro * 4.16
+        gasto_total_consumo = gnv
+    }
+    
+    gasto_total.innerHTML = ` ${gasto_total_consumo.toFixed(2)}`
+}
+
+function limpar() {
+    gasto_total.innerHTML = 0
+    input_km_viagem.value = ''
+    input_km_carro.value = ''
 }
